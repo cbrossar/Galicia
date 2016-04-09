ec573c037f2ef458381690d62557bcf0b0994cc4
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2016-04-08 18:42:24 -0700

    Falling cookies

--- initialGame/core/src/com/maxaer/constants/GameConstants.java
+++ initialGame/core/src/com/maxaer/constants/GameConstants.java
@@ -10,4 +10,15 @@ public class GameConstants
    public static final short MASK_PLAYER = CATEGORY_PLATFORM; //A player should be able to make contact with a block 
    public static final short MASK_BLOCK = CATEGORY_PLAYER | CATEGORY_BLOCK | CATEGORY_PLATFORM; // A block should only make contact with a block
    public static final short MASK_PLATFORM = CATEGORY_BLOCK | CATEGORY_PLAYER;
+   
+   //Image resources 
+   public static final String ThinMintImg = "Cookies/ThinMint.png";
+   public static final String DoSiDoImg = "Cookies/DoSiDo.png";
+   public static final String BerryImg = "Cookies/Berry.png";
+   public static final String TagalongImg = "Cookies/Tagalong.png";
+   public static final String LemonImg = "Cookies/Lemon.png";
+   public static final String SamoaImg = "Cookies/SamoaFlake.png";
+   public static final String ThanksALotImg = "Cookies/ThanksALot.png";
+   public static final String TrefoilImg = "Cookies/Trefoil72x72.png";
+   
 }
