9e08a328e8c965f797afef49f58da69582ce9a06
Carl Eubanks <ceubanks@usc.edu>
2016-04-11 01:51:38 -0700

    Jesus christ

--- initialGame/core/src/com/maxaer/game/UserInputListener.java
+++ initialGame/core/src/com/maxaer/game/UserInputListener.java
@@ -11,7 +11,6 @@ import com.maxaer.gameworld.GameWorld;
  * Purpose: This class is where any user input will be processed
  *          1. This listener needs to be passed an instance of the GameWorld so it can interact with the player
  */
-<<<<<<< HEAD
 public class UserInputListener implements InputProcessor {
 	private GameWorld world;
 	private Body player;
@@ -105,114 +104,5 @@ public class UserInputListener implements InputProcessor {
 		// TODO Auto-generated method stub
 		return false;
 	}
-=======
-public class UserInputListener implements InputProcessor
-{  
-   private GameWorld world;
-   private Body player;
-   //Pass the constructor an instance of our GameWorld so we can manipulate the game objects
-   public UserInputListener(GameWorld world)
-   {
-      this.world = world;
-      player = world.getPlayerBody();
-   }
-
-   @Override
-   public boolean keyDown(int keycode)
-   {
-	   
-	   	if(keycode == Input.Keys.UP && !world.isGameOver()){
-	   		if(world.getPlayer().canJump()){
-	   			player.applyForceToCenter(0f, -2.5f,true);
-	   			world.getPlayer().setJumpability(false);
-	   		}
-	   	}
-	   	
-	   	if(keycode == Input.Keys.ENTER && world.isGameOver()){
-	   	   world.showMenuScreen(); 
-	   	}
-	   	
-	   	if(keycode == Input.Keys.RIGHT && !world.isGameOver()){   		
-	        player.setLinearVelocity(3f, player.getLinearVelocity().y);
-	    }
-	      
-	    if(keycode == Input.Keys.LEFT && !world.isGameOver()){
-	        player.setLinearVelocity(-3f,player.getLinearVelocity().y);
-	    }
-	     
-	    return true;
-	
-   }
-
-
-   @Override
-   public boolean keyUp(int keycode) {
-      
-      //If the game is over, the user can press space to create a new game session
-      if(keycode == Input.Keys.SPACE && world.isGameOver()){
-         world.createNewGame(); 
-      }
-	  
-
-       if(keycode == Input.Keys.RIGHT && !world.isGameOver()){
-    	   player.setLinearVelocity(0, player.getLinearVelocity().y * 1f);
-       }
-       if(keycode == Input.Keys.LEFT && !world.isGameOver())
-    	  player.setLinearVelocity(0, player.getLinearVelocity().y * 1f);
-
-       if(keycode == Input.Keys.UP && !world.isGameOver()){
-    	   if(player.getLinearVelocity().y == 0)
-    		   player.applyForceToCenter(0f, -2.5f,true);
-       }
-       
-          
-
-       return true;
-   }
-
-
-   @Override
-   public boolean keyTyped(char character)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean touchDown(int screenX, int screenY, int pointer, int button)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean touchUp(int screenX, int screenY, int pointer, int button)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean touchDragged(int screenX, int screenY, int pointer)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean mouseMoved(int screenX, int screenY)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean scrolled(int amount)
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-   
->>>>>>> fb1e73ce4c3cce0e6b8ed4b7aba89869cab00718
 
 }
